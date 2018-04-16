5472d398b9e6fd261ed028396c4ebc0d6a40dfca
Steven Chen <ysteven.chen2@gmail.com>
2018-04-16 20:41:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var recipes={};
 function updateObjectWithKeyAndValue(object,key,value){
 return Object.assign({},object,{[key]:value});
@@ -15,3 +16,6 @@ function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key];
   return object;
 }
+=======
+var object=recipes;
+>>>>>>> 10146bf60c64cbf81b022cb1c37e1a785245c3fa
